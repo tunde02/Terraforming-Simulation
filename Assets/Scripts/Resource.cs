ea1044da63dc9167a2850d171b5b84263835ab0e
SeongHo Jin <snskrktja@naver.com>
2020-12-25 07:02:36 +0900

    Add Resource class

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Resource
+{
+    private readonly string[] units = { "", "a", "b", "c", "d", "e", "f" };
+
+    public ResourceID resourceId;
+    public string name;
+    public long storage;
+    public long income;
+
+    public Resource(ResourceID resourceId, string name, long storage, long income)
+    {
+        this.resourceId = resourceId;
+        this.name = name;
+        this.storage = storage;
+        this.income = income;
+    }
+
+    public override string ToString()
+    {
+        double storageOverview;
+        double incomeOverview;
+        int storageUnit = 0;
+        int incomeUnit = 0;
+        double storageCompare = 1000;
+        double incomeCompare = 1000;
+
+        while (storage >= storageCompare)
+        {
+            storageCompare *= 1000;
+            storageUnit++;
+        }
+
+        while (income >= incomeCompare)
+        {
+            incomeCompare *= 1000;
+            incomeUnit++;
+        }
+
+        storageOverview = Math.Floor(storage / (storageCompare / 1000) * 10) * 0.1d;
+        incomeOverview = Math.Floor(income / (incomeCompare / 1000) * 10) * 0.1d;
+
+        return $"{storageOverview}{units[storageUnit]} + {incomeOverview}{units[incomeUnit]}";
+    }
+}

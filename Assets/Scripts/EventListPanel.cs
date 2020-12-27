c41a10c1f69947e0a7b8209ae5dd393a5b8b0694
SeongHo Jin <snskrktja@naver.com>
2020-12-27 21:36:49 +0900

    Add EventListPanel and simple interaction

---
+++ Assets/Scripts/EventListPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventListPanel : SideMenuPanel
+{
+    private RectTransform rectTransform;
+    private readonly float inScreenPosX = 150f;
+    private readonly float outScreenPosX = 935f;
+
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public override void MoveIntoScreen()
+    {
+        rectTransform
+            .DOAnchorPosX(inScreenPosX, 0.5f)
+            .SetEase(Ease.OutExpo);
+    }
+
+    public override void MoveOutOfScreen()
+    {
+        rectTransform
+            .DOAnchorPosX(outScreenPosX, 0.5f)
+            .SetEase(Ease.InExpo);
+    }
+}
---
+++ Assets/Scripts/SideMenuPanel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SideMenuPanel : MonoBehaviour
+{
+    public abstract void MoveIntoScreen();
+    public abstract void MoveOutOfScreen();
+}

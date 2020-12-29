5d9408761da96a595b28d228577547a290272205
SeongHo Jin <snskrktja@naver.com>
2020-12-29 17:23:07 +0900

    Add Action.cs

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public enum ActionType
+{
+    Breed,
+    Hunt,
+    Evolve,
+    Train
+}
+
+public class Action
+{
+    public ActionType Type { get; }
+    public long Income { get; set; }
+    public float Weight { get; set; }
+    public long Consumption { get; set; }
+
+    public Action(ActionType type)
+    {
+        Type = type;
+
+        switch (Type)
+        {
+            case ActionType.Breed:
+                Income = 100;
+                Consumption = 0;
+                break;
+            case ActionType.Hunt:
+                Income = 10;
+                Consumption = 30;
+                break;
+            case ActionType.Evolve:
+                Income = 2;
+                Consumption = 50;
+                break;
+            case ActionType.Train:
+                Income = 10;
+                Consumption = 50;
+                break;
+        }
+
+        Weight = 1.0f;
+    }
+
+    public List<(ResourceType resourceType, long amount)> PerformAction(Resource necessaryResource, Resource targetResource)
+    {
+        var variations = new List<(ResourceType resourceType, long amount)>();
+
+        necessaryResource.Storage -= Consumption;
+        targetResource.Storage += (long)(Income * Weight);
+
+        variations.Add((necessaryResource.resourceType, -Consumption));
+        variations.Add((targetResource.resourceType, (long)(Income * Weight)));
+
+        return variations;
+    }
+}
